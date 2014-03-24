7af2f85589b36e46a5ebd12ec8ab1e762a9a87d5
Andrei Karpushonak <andrei@avrora.io>
2014-03-24 16:55:12 +0100

    use european registry

--- configure.sh
+++ configure.sh
@@ -3,6 +3,9 @@
 #configuring the system
 wget https://raw.github.com/avrora/travis-ci-laika/master/Makefile
 
+# set european mirror
+npm set registry  http://registry.npmjs.eu/
+
 #install meteor
 curl https://install.meteor.com | /bin/sh
 
